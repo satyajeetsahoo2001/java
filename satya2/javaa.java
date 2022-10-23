b38a6dc634378710c6d897164fd2caab90fdbc9b
satyajeet sahoo <ssatyajit552@gmail.com>
2022-10-23 10:53:52 +0530

    this is the 3rd try to enter in git

---
+++ satya2/javaa.java
@@ -0,0 +1,13 @@
+package satya2;
+
+import java.util.*;
+
+public class javaa {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+
+        System.out.println(a);
+    }
+}
