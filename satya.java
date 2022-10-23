a67bedab3f2301f68a2c20cc083d01ba357ff062
satyajeet sahoo <ssatyajit552@gmail.com>
2022-10-23 10:57:10 +0530

    third try

--- satya.java
+++ satya.java
@@ -3,7 +3,7 @@ public class satya {
     public static void main( String[] args){
         System.out.println("hii");
         System.out.println("hii");
-        System.out.println("hii");
+
         System.out.println("hii");
 
 
